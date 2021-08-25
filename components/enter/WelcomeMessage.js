a8b7e2e370da0134e44071506073c6704e7f8a33
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-08-26 00:37:48 +0530

    Implement welcome message after auth

--- components/enter/WelcomeMessage.js
+++ components/enter/WelcomeMessage.js
@@ -1,8 +1,15 @@
+import { Heading, Text, VStack } from '@chakra-ui/react';
+
 const WelcomeMessage = () => {
   return (
-    <>
-      <p>Hello from WelcomeMessage</p>
-    </>
+    <VStack spacing={4} mx={16} align="flex-start">
+      <Heading color="gray.700">Welcome to Intermediate! 🎉</Heading>
+      <Text color="gray.600">
+        Head on over to your profile to get started with your first blog post.
+        If you&#39;re more more of a lurker, you can check out blogs by your
+        favorite authors on the home page. Have fun!
+      </Text>
+    </VStack>
   );
 };
 
