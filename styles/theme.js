604685574393b1cf852f7ca7c9ab1f1ff6a907c6
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-08-25 16:44:15 +0530

    Remove focus boxes from buttons and links

--- styles/theme.js
+++ styles/theme.js
@@ -1,5 +1,27 @@
 import { extendTheme } from '@chakra-ui/react';
 
+const Link = {
+  baseStyle: {
+    _focus: {
+      boxShadow: 'none',
+    },
+    _focusVisible: {
+      boxShadow: 'outline',
+    },
+  },
+};
+
+const Button = {
+  baseStyle: {
+    _focus: {
+      boxShadow: 'none',
+    },
+    _focusVisible: {
+      boxShadow: 'outline',
+    },
+  },
+};
+
 const theme = extendTheme({
   styles: {
     global: {
@@ -16,6 +38,7 @@ const theme = extendTheme({
   fonts: {
     body: `-apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji"`,
   },
+  components: { Button, Link },
 });
 
 export default theme;
