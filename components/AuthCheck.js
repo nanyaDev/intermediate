02152d10c6f0c760cd88ac3e650d3dfbcb8ff873
Rishabh Chatterjee <nanya.login@protonmail.com>
2021-09-01 13:12:57 +0530

    Implement AuthCheck

---
+++ components/AuthCheck.js
@@ -0,0 +1,24 @@
+import NextLink from 'next/link';
+import { Center, Text, Link } from '@chakra-ui/layout';
+
+import { useAuth } from '@/lib/auth';
+
+const AuthCheck = ({ children }) => {
+  const { username } = useAuth();
+
+  return username ? children : <SignInPrompt />;
+};
+
+const SignInPrompt = () => (
+  <Center h="full">
+    <Text fontSize="18px">
+      You must be{' '}
+      <NextLink href="/enter" passHref>
+        <Link color="blue.500">signed in</Link>
+      </NextLink>{' '}
+      to access this page!
+    </Text>
+  </Center>
+);
+
+export default AuthCheck;
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,11 @@
+import AuthCheck from '@/components/AuthCheck';
+
 const Admin = () => {
   // pass
   return (
-    <>
+    <AuthCheck>
       <h1>Hello from Admin!</h1>
-    </>
+    </AuthCheck>
   );
 };
 
